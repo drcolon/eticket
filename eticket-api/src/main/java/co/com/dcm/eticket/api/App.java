e5fb1118bae7b6b5dbf8b856d0f259e716b36ecf
DanielCM <DanielCM@192.168.1.10>
2014-10-09 23:07:07 -0500

    init project

---
+++ eticket-api/src/main/java/co/com/dcm/eticket/api/App.java
@@ -0,0 +1,13 @@
+package co.com.dcm.eticket.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
